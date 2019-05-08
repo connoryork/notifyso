220d73bc7248823ec2cb8505352c9b4f372b637a
cyork <cxy1054@rit.edu>
2019-05-07 20:51:39 -0400

    added templating

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template('index.html')
+
+@app.route('/sent')
+def sent():
+    return render_template('sent.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
